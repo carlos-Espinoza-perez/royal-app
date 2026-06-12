f5de1a76c8b79a0190c69ab7a31177243f605be3
Carlos Espinoza <cpespinoza044@gmail.com>
2026-06-12 17:56:20 -0600

    feat: configure PWA and add application logo

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Royal App',
+        short_name: 'RoyalApp',
+        description: 'Royal Treasury web app',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   server: {
     allowedHosts: true
   }
